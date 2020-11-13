94579735881ddd3fd46157a9e9dbdaa9b4b2a42c
Alan Tam <alantam43@gmail.ca>
2020-11-12 23:05:11 -0500

    added Font Awesome icons.

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Alan Tam - Personal Website')
+    return render_template('index.html', title='Alan Tam')
