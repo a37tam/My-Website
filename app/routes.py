9d0b5c91069d1a77d62490de9cb79b5f05dba058
Alan Tam <alantam43@gmail.ca>
2020-11-12 19:24:11 -0500

    add base layout

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-def home():
-    return "This is the homepage."
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Alan Tam - Personal Website')
